10ff85a6eca2dcd8dc8879f31fcc2a999e9eba5a
pranith44 <66859567+pranith44@users.noreply.github.com>
2020-08-20 22:54:01 +0530

    code

---
+++ scr.py
@@ -0,0 +1,41 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as soup
+from urllib.error import HTTPError
+myurl="https://play.google.com/store/apps?hl=en"
+req=urlopen(myurl)
+page_1=req.read()
+req.close()
+page_2=soup(page_1,"html.parser")
+categories=page_2.findAll("a",{"class":"r2Osbf"})
+hrefs_cat=[]
+names=[]
+hrefs_sim=[]
+try:
+
+    for i in range(len(categories)):
+        hrefs_cat.append(categories[i]["href"])
+    for href in hrefs_cat:
+        opurl=urlopen("https://play.google.com"+href)
+        page=opurl.read()
+        opurl.close()
+        soup_page=soup(page,"html.parser")
+        apps=soup_page.findAll("div",{"class":"WsMG1c nnK0zc"})
+        similars=soup_page.findAll("a",{"class":"LkLjZd ScJHi U8Ww7d xjAeve nMZKrb id-track-click"})
+        for i in range(len(similars)):
+            hrefs_sim.append(similars[i]["href"])
+        for hrf in hrefs_sim:
+            opurl_1=urlopen("https://play.google.com"+hrf)
+            page_3=opurl_1.read()
+            opurl_1.close()
+            sim_soup_page=soup(page_3,"html.parser")
+            sim_apps=sim_soup_page.findAll("div",{"class":"WsMG1c nnK0zc"})
+            for i in range(len(sim_apps)):
+                names.append(sim_apps[i]["title"])
+                print(sim_apps[i]["title"])
+except HTTPError as err:
+    if err.code==404:
+        pass
+    else:
+        raise
+names_set=set(names)
+print(len(names_set))
